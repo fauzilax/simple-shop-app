51d1cd5ec48486288629dc5b33bb941723d7598d
Fauzi Sofyan <fauzilax@gmail.com>
2023-03-10 23:56:54 +0700

    learn: calling funct and class

---
+++ src/component/HelloComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './HelloComponent.css'
+
+
+// function FuncComponent(){
+//   return <p>HelloWorld</p>
+// }
+class StatefulComponent extends React.Component{
+    render(){
+      return <p className="hello"><h1>HelloWorld Fauzi</h1></p>
+    }
+}
+
+export default StatefulComponent
